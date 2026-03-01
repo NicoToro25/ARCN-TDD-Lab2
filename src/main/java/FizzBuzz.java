c2ead64416c1cb257ce262d1faaa49e6be4e4541
Nicolas Toro <158247190+NicoToro25@users.noreply.github.com>
2026-03-01 02:30:45 +0000

    Corrige errores tipográficos en la implementación de FizzBuzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -2,13 +2,13 @@ public class FizzBuzz {
 
     public static String fizzbuzz(int n) {
         if (n % 3 == 0 && n % 5 == 0) {
-            return "FizBuzz";
+            return "FizzBuzz";
         } else if (n % 3 == 0) {
             return "Fizz";
         } else if (n % 5 == 0) {
             return "Buzz";
         } else {
-            return String.valueOf(0);
+            return Integer.toString(n);
         }
     }
 }
